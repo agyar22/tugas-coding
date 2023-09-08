9c7fe05db46cdbd3a4bd210e1a8520c940c1c537
agyar22 <143859804+agyar22@users.noreply.github.com>
2023-09-08 15:24:45 +0700

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,12 @@
+p = int(input("masukkan panjang : "))
+l = int(input("masukkan lebar :"))
+t = int(input("masukkan tinggi : "))
+
+volume = int( p*l*t )
+
+print("menghitung volume balok :" ,volume)
+
+
+
+
+
---
+++ project2.py
@@ -0,0 +1,12 @@
+r =int(input("masukkan jari-jari :" ))
+t =int(input("masukkan tinggi :"))
+
+volume = r*t
+
+print("menghitung volume tabung :" ,volume)
+ 
+ 
+
+
+
+
